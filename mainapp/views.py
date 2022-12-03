03db740e4e63358539e44c44627bc644fb13ea53
sarwar-hsn <sarwar.hossain2016@gtu.edu.tr>
2022-12-03 12:46:28 +0300

    homepage updated

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from tour.models import Tour
+from tour.models import Tour,Destination
 from newsletterapp.forms import NewsletterForm
+
+
 # Create your views here.
+def popular_destinations(city1, city2 , city3):
+    dest_one = Destination.objects.filter(city=city1)[0]
+    dest_set_one = Tour.objects.filter(destinations__city=city1)
+    dest_two = Destination.objects.filter(city=city2)[0]
+    dest_set_two = Tour.objects.filter(destinations__city=city2)
+    dest_three = Destination.objects.filter(city=city3)[0]
+    dest_set_three = Tour.objects.filter(destinations__city=city3)
+    return {dest_one:dest_set_one,dest_two:dest_set_two,dest_three:dest_set_three}
+    
 def index(request,*args, **kwargs):
     banners = Tour.objects.all()
-    populardestinations = popular_destinations("istanbul", "gebze", "antalya")
+    CITY1='istanbul'
+    CITY2='antalya'
+    CITY3='edirne'
+    populardestinations = popular_destinations(CITY1,CITY2,CITY3)
     featured= feautured_packages()
     context = {
         "banners":banners,
@@ -26,12 +40,7 @@ def bestpackages():
         packages = packages[0:6]
     return packages
 
-def popular_destinations(city1, city2 , city3):
-    dest_set_one = Tour.objects.filter(destinations__city=city1)
-    dest_set_two = Tour.objects.filter(destinations__city=city2)
-    dest_set_three = Tour.objects.filter(destinations__city=city3)
-    return {city1:dest_set_one,city2:dest_set_two,city3:dest_set_three}
-    
+
 def feautured_packages():
     return Tour.objects.filter(featured=True)
     

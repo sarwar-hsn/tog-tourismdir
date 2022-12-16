eef77f06619b2b1dca910c6f946c05499377ed2b
sarwar hossain <64771642+sarwar-hsn@users.noreply.github.com>
2022-12-16 16:00:59 +0300

    Delete static directory

--- static/analyticsapp/signals.py
+++
@@ -1,9 +0,0 @@
-from django.dispatch import Signal
-
-
-object_view_signal = Signal()
-
-
-
-
-
--- static/newsletterapp/urls.py
+++
@@ -1,9 +0,0 @@
-import uuid
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('sub/',views.subscribe,name='newsletter-subscribe'),
-    path('unsub/',views.unsubsribe,name='newsletter-unsubscribe')
-]

108914f482d79dd11a735a16b825f75ab7061d74
sarwar-hsn <sarwar.hossain2016@gtu.edu.tr>
2022-12-17 12:46:54 +0300

    prod static fixed

--- togtourismsite/settings.py
+++ togtourismsite/settings.py
@@ -171,13 +171,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/'staticfiles'
-STATICFILES_DIRS = [
-    BASE_DIR / "",
-]
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL ='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'mediafiles')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 
 # Default primary key field type

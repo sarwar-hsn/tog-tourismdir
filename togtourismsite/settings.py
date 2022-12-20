2a87e72dd3e566f7fbf233f06d9a3e6f3b6ff9c3
sarwar-hsn <sarwar.hossain2016@gtu.edu.tr>
2022-12-20 23:07:27 +0300

    prod with new db

--- togtourismsite/settings.py
+++ togtourismsite/settings.py
@@ -88,6 +88,19 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'togtourismsite.wsgi.application'
+
+# if DEBUG is True:
+#     if 'DATABASE_URL' in os.environ:
+#         DATABASES = {
+#             'default': dj_database_url.parse('host=tourismapp-server.postgres.database.azure.com port=5432 dbname=tourismapp-database user=hrsimzyqxt password=#ottoman2022@DB sslmode=require')
+#         }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 # DATABASES = {

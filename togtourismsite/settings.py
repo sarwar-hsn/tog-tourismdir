bd0bea59f42ef56c2ae3875142c0035d1f552d68
sarwar-hsn <sarwar.hossain2016@gtu.edu.tr>
2022-12-21 23:06:32 +0300

    prod ckeditor 2

--- togtourismsite/settings.py
+++ togtourismsite/settings.py
@@ -182,7 +182,7 @@ CKEDITOR_CONFIGS = {
         'width': 1200,
     },
 }
-CKEDITOR_BASEPATH = "static/ckeditor/ckeditor/"
+# CKEDITOR_BASEPATH = "static/ckeditor/ckeditor/"
 
 THUMBNAIL_ALTERNATIVE_RESOLUTIONS = [2,3,]
 

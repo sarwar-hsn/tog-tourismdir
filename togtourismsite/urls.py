e7b39b4d827d967f332965fb955cdc370cb3d04e
sarwar-hsn <sarwar.hossain2016@gtu.edu.tr>
2022-12-21 23:55:34 +0300

    prod ckeditor 4

--- togtourismsite/urls.py
+++ togtourismsite/urls.py
@@ -42,6 +42,7 @@ urlpatterns = [
     path('tours/',include(toururls)),
     path('auth/',include('django.contrib.auth.urls')),
     path('newsletter/',include(newsletterurls)),
+    path('ckeditor/', include('ckeditor.urls')),
     path('',include(mailappappurls)),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
          name='django.contrib.sitemaps.views.sitemap'),

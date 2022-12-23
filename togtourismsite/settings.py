7411d7dc0d6fe5ede544a504bfaf2895033d6be2
sarwar-hsn <sarwar.hossain2016@gtu.edu.tr>
2022-12-23 11:59:30 +0300

    branch updated

--- togtourismsite/settings.py
+++ togtourismsite/settings.py
@@ -24,10 +24,11 @@ AUTH_USER_MODEL = 'authentication.User'
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)=="True"
 
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+CSRF_TRUSTED_ORIGINS = ["https://"+os.environ.get('DJANGO_ALLOWED_HOSTS')]
 
 
 # Application definition
@@ -142,9 +143,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-AZURE = (os.environ.get('AZURE_STORAGE_KEY',None))
-
-if AZURE is not None:
+if DEBUG is False:
     AZURE_STORAGE_KEY = os.environ.get('AZURE_STORAGE_KEY', False)
     AZURE_ACCOUNT_NAME = "ottomangrpstorage"  # your account name
     AZURE_MEDIA_CONTAINER = os.environ.get('AZURE_MEDIA_CONTAINER', 'media')
@@ -185,15 +184,16 @@ CKEDITOR_CONFIGS = {
     },
 }
 
+CKEDITOR_BASEPATH = "static/ckeditor/ckeditor/"
 
 THUMBNAIL_ALTERNATIVE_RESOLUTIONS = [2,3,]
 
-# EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') 
-# EMAIL_READY=(EMAIL_HOST_USER is not None and EMAIL_HOST_PASSWORD is not None)
-# if EMAIL_READY:
-#     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-#     EMAIL_HOST = 'smtp.gmail.com'
-#     EMAIL_PORT = 587
-#     EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') 
+EMAIL_READY=(EMAIL_HOST_USER is not None and EMAIL_HOST_PASSWORD is not None)
+if EMAIL_READY:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
 

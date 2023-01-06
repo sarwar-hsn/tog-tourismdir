343a55e0cceff95c1c9511180c490114f57603b9
sarwar-hsn <sarwar.hossain2016@gtu.edu.tr>
2023-01-06 14:48:16 +0300

    db cache added done

--- togtourismsite/settings.py
+++ togtourismsite/settings.py
@@ -24,8 +24,8 @@ AUTH_USER_MODEL = 'authentication.User'
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get('DEBUG', False)=="True"
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)=="True"
+# DEBUG = True
 
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")

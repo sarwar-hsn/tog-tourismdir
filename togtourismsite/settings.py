e8e98ee7a3a901ff03379f301b4ce13370148726
sarwar-hsn <sarwar.hossain2016@gtu.edu.tr>
2023-01-21 18:24:19 +0300

    domain added

--- togtourismsite/settings.py
+++ togtourismsite/settings.py
@@ -29,7 +29,7 @@ DEBUG = os.environ.get('DEBUG', False)=="True"
 
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
-# CSRF_TRUSTED_ORIGINS = ["https://"+os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1")]
+CSRF_TRUSTED_ORIGINS = ["https://"+os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1")]
 
 
 # Application definition
